275e7c3b6de501365ae4b3fd46ea8837f891093c
CharlesPhilippeLabbe <c.p@o-possum.com>
2026-03-07 09:40:44 -0500

    Removed dependency to echo

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"io"
 	"log"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"slices"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"time"
 )
 
 type Templates struct {
 	templates *template.Template
 }
 
-func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) error {
+func (t *Templates) Render(w io.Writer, name string, data any) error {
 
 	return t.templates.ExecuteTemplate(w, name, data)
 }
@@ -110,11 +108,8 @@ func newPage() Page {
 
 func main() {
 
-	e := echo.New()
-	e.Use(middleware.Logger())
-
 	page := newPage()
-	e.Renderer = newTemplate()
+	templates := newTemplate()
 
 	target, err := url.Parse("http://localhost:1313")
 	if err != nil {
@@ -122,40 +117,44 @@ func main() {
 	}
 	proxy := httputil.NewSingleHostReverseProxy(target)
 
-	e.Any("/*", func(c echo.Context) error {
-		req := c.Request()
+	mux := http.NewServeMux()
 
-		proxy.ServeHTTP(c.Response().Writer, req)
-		return nil
+	// Registering a handler for a specific method and path with a variable
+	mux.HandleFunc("/{path...}", func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
 	})
 
-	e.GET("/api", func(c echo.Context) error {
-		err := c.Render(200, "index", page)
+	render := func(w http.ResponseWriter, t string, d any) {
+		err := templates.Render(w, t, d)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		return err
+	}
+
+	mux.HandleFunc("GET /api", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "index", page)
 	})
 
-	e.POST("/api/contacts", func(c echo.Context) error {
-
-		name := c.FormValue("name")
+	mux.HandleFunc("POST /api/contacts", func(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("name")
 		if page.Data.Todo.hasTask(name) >= 0 {
 			formData := newFormData()
 			formData.Values["name"] = name
 			formData.Errors["email"] = "Already exists"
 
-			return c.Render(422, "form", formData)
+			w.WriteHeader(422)
+			render(w, "form", formData)
+			return
 		}
 		contact := newTask(name)
 		page.Data.Todo.Tasks = append(page.Data.Todo.Tasks, contact)
 
-		c.Render(200, "form", newFormData())
-		return c.Render(200, "oob-contact", contact)
+		render(w, "form", newFormData())
+		render(w, "oob-contact", contact)
 	})
-
-	e.PUT("/api/task/:name", func(c echo.Context) error {
-		name := c.Param("name")
+	mux.HandleFunc("PUT /api/task/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := r.PathValue("name")
 		if page.Data.Todo.deleteTask(name) {
 			if page.Data.Doing.hasTask(name) < 0 {
 				page.Data.Doing.Tasks = append(page.Data.Doing.Tasks, newTask(name))
@@ -165,19 +164,30 @@ func main() {
 				page.Data.Done.Tasks = append(page.Data.Done.Tasks, newTask(name))
 			}
 		} else if !page.Data.Done.deleteTask(name) {
-			return c.HTML(404, "not found")
+			w.WriteHeader(http.StatusNotFound)
+			_, err := w.Write([]byte("not found"))
+			if err != nil {
+				log.Println(err)
+			}
+			return
 		}
-		return c.Render(200, "display", page.Data)
+		render(w, "display", page.Data)
 	})
 
-	e.DELETE("/data", func(c echo.Context) error {
+	mux.HandleFunc("DELETE /data", func(w http.ResponseWriter, r *http.Request) {
 		page = newPage()
-		err := c.Render(200, "index", page)
-		if err != nil {
-			fmt.Println(err)
-		}
-		return err
+		render(w, "index", page)
 	})
 
-	e.Logger.Fatal(e.Start(":6969"))
+	err = http.ListenAndServe(":8080", LoggingMiddleware(mux))
+	if err != nil {
+		log.Println(err)
+	}
+}
+func LoggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+        next.ServeHTTP(w, r)
+        log.Printf("[%s] %s %s took %v\n", r.Method, r.URL.Path, r.Proto, time.Since(start))
+    })
 }
